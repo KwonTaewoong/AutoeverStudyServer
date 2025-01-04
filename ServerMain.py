0db1620f30dac6481b9251a1e26cfbb3682bce72
DESKTOP-A7E8OMT\User <ktwkjb@gmail.com>
2025-01-04 12:51:20 +0900

    추가 : 그리드 세팅 함수 구현

--- ServerMain.py
+++ ServerMain.py
@@ -25,6 +25,28 @@ def main():
 	if obstacle < 1 or obstacle > greedSize - 1:
 		print ("obstacle is smaller than greedSize")
 		exit()
+	greed = CreateGreed(greedSize)
+	greed = SetObstacleOnGreed(greed, obstacle)
+
+
+def CreateGreed(greedSize):
+	greed = np.zeros((greedSize, greedSize))
+	return greed
+
+
+def SetObstacleOnGreed(greed, obstacle):
+	temp = greed[1:len(greed)-1,:]
+	originShape = temp.shape
+	temp = temp.ravel()
+	index = np.random.choice(temp.shape[0], obstacle, False)
+	for i in index:
+		temp[i] = 1
+	temp = temp.reshape(originShape)
+	greed[1:len(greed)-1, :] = temp
+	print(greed)
+
+	return greed
+
 
 if __name__ == "__main__":
 	main()
