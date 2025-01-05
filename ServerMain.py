083b7207952eace67b998ffe2584ca66aaf7580b
DESKTOP-7F3OE15\Admin <ktwkjb@gmail.com>
2025-01-05 13:56:25 +0900

    추가 : 서버에서 그리드 길이, 장애물 숫자를 클라이언트로 전달 기능

--- ServerMain.py
+++ ServerMain.py
@@ -3,18 +3,32 @@ import sys
 import datetime
 from simple_websocket_server import WebSocketServer, WebSocket
 
-class SimpleEcho(WebSocket):
-	def handle(self):
-		self.send_message(self.data)
-		print(self.data)
 
-	def connected(self):
-		print(self.address, 'connected')
 
-	def handle_close(self):
-		print(self.address, 'closed')
 
 def main():
+	class SimpleEcho(WebSocket):
+		greed = None
+		obstacle = None
+
+		def SetData(Greed, Obstacle):
+			greed = Greed
+			obstacle = Obstacle
+
+		def handle(self):
+			print(self.data)
+			if (self.data == "GetGreed"):
+				self.send_message(str(len(greed)))
+			elif (self.data == "GetObs"):
+				self.send_message(str(obstacle))
+
+
+		def connected(self):
+			print(self.address, 'connected')
+
+		def handle_close(self):
+			print(self.address, 'closed')
+
 	if len(sys.argv) != 3:
 		print ("argument size is not 2")
 		exit()
@@ -37,15 +51,14 @@ def main():
 	if obstacle < 1 or obstacle > greedSize - 1:
 		print ("obstacle is smaller than greedSize")
 		exit()
+
 	greed = CreateGreed(greedSize)
+
 	greed = SetObstacleOnGreed(greed, obstacle)
 
-	OpenServerWebsocket()
-
-	
-
-def OpenServerWebsocket():
 	server = WebSocketServer('localhost', 3000, SimpleEcho)
+	SimpleEcho.SetData(greed, obstacle)
+
 	server.serve_forever()
 
 
@@ -63,7 +76,7 @@ def SetObstacleOnGreed(greed, obstacle):
 		temp[i] = 1
 	temp = temp.reshape(originShape)
 	greed[1:len(greed)-1, :] = temp
-	print(greed)
+
 
 	return greed
 
